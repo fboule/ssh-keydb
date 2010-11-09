551abf38683a191eec8ab1d27099458d8379af1d
fabien.bouleau <fabien.bouleau@f3925952-77d6-1dbf-3d77-c495bd4bd6e4>
2010-11-09 21:51:18 +0000

    Moved the control files away from the plugins folder.
    git-svn-id: http://ssh-keydb.googlecode.com/svn/trunk@16 f3925952-77d6-1dbf-3d77-c495bd4bd6e4

--- ssh_keydb/options.py
+++ ssh_keydb/options.py
@@ -20,7 +20,7 @@
 import sys
 import getopt
 from plugins.help import HelpController
-from plugins.controller import *
+from controller import *
 
 class OptionsController(Controller):
     _g_shortopts = 'd:'
